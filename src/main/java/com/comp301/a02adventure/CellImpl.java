61b58aab9811ec7d06c7ac84dc99d98fe2bb9d19
TaoHuang0 <thuang@unc.edu>
2022-02-11 00:47:41 -0500

    V0.7

---
+++ src/main/java/com/comp301/a02adventure/CellImpl.java
@@ -0,0 +1,86 @@
+package com.comp301.a02adventure;
+
+public class CellImpl implements Cell {
+  private Position pos;
+  private String name;
+  private String description;
+  private Inventory chest;
+  private boolean flag;
+
+  public CellImpl(int x, int y, String name, String description) {
+    this(x, y);
+    this.name = name;
+    this.description = description;
+    this.flag = false;
+    if (name.equals(null) || description.equals(null)) {
+      throw new IllegalArgumentException("No name or no description");
+    }
+    chest = null;
+  }
+
+  public CellImpl(int x, int y) {
+    pos = new PositionImpl(x, y);
+    this.name = null;
+    this.description = null;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public String getDescription() {
+    return this.description;
+  }
+
+  public Position getPosition() {
+    return pos;
+  }
+
+  public Inventory getChest() {
+    if (this.chest != null) {
+      return chest;
+    } else {
+      throw new IllegalArgumentException("No chest exist");
+    }
+  }
+
+  public boolean getIsVisited() {
+    return flag;
+  }
+
+  public boolean hasChest() {
+    if (this.chest != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public void setName(String name) {
+    if (name != null) {
+      this.name = name;
+    } else {
+      throw new IllegalArgumentException("Name not valid");
+    }
+  }
+
+  public void setDescription(String description) {
+    if (description != null) {
+      this.description = description;
+    } else {
+      throw new IllegalArgumentException("Name not valid");
+    }
+  }
+
+  public void setChest(Inventory chest) {
+    if (chest != null) {
+      this.chest = chest;
+    } else {
+      throw new IllegalArgumentException("Name not valid");
+    }
+  }
+
+  public void visit() {
+    flag = true;
+  }
+}
