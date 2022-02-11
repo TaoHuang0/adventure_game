8e623b9585364512dbcca5537429ab9b7db4b030
TaoHuang0 <thuang@unc.edu>
2022-02-10 20:07:02 -0500

    V0.3

--- src/main/java/com/comp301/a02adventure/ItemImpl.java
+++ src/main/java/com/comp301/a02adventure/ItemImpl.java
@@ -16,8 +16,14 @@ public class ItemImpl implements Item {
   }
 
   public boolean equals(Object other) {
+    if (other == this) {
+      return true;
+    }
+    if (!(other instanceof ItemImpl)) {
+      return false;
+    }
     ItemImpl temp = (ItemImpl) other;
-      return this.name.equals(temp.name);
+    return this.name.equals(temp.name);
   }
 
   public String toString() {
